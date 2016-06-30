53a24e4af99659f545233d688f80076a4e18b681
akiladevi11 <akiladevi@etekchnoservices.com>
2016-06-30 16:27:19 +0530

    Updated & Minified Folder -Akila

--- footer.php
+++ footer.php
@@ -1,44 +1,34 @@
 <div class="complete-footer">
             <footer id="footer">
-        		<div class="container">
-            		<div class="row">
-                    	<!--Foot widget-->
-                    	<div class="col-xs-12 col-sm-4 col-md-4 foot-widget image_centering">
-                        	<a href="index.php"><div class="footer_logo_align"><img src="images/imed-logos-sm.png" class="logo_chem_bottom"/></div></a>
-	                       
+                <div class="container">
+                    <div class="row">
+                        <!--Foot widget-->
+                        <div class="col-xs-12 col-sm-4 col-md-4 foot-widget image_centering">
+                            <a href="index.php"><div class="footer_logo_align"><img src="images/imed-logos-sm.png" class="logo_chem_bottom"/></div></a>
+                           
                         </div>
                         <!--Foot widget-->
                         <div class="col-xs-12 col-sm-8 col-md-8 recent-post-foot foot-widget ">
-                        	<div class="foot-widget-title">
-                        		An initiative of the International Organization for Chemical Sciences in Development
-                        		<p style="font-size: 12px;text-align: center!important;"><a href="http://www.iocd.org" target="_blank">www.iocd.org</a><a href="index.php">www.oneworldchemistry.org</a></p>
-                        	</div>
-                        	
+                            <div class="foot-widget-title">
+                                An initiative of the International Organization for Chemical Sciences in Development
+                                <p style="font-size: 12px;text-align: center!important;"><a href="http://www.iocd.org" target="_blank">www.iocd.org</a><a href="index.php">www.oneworldchemistry.org</a></p>
+                            </div>
+                            
                         </div>
                     </div>
                     <div class="copy_right_holder">
                     <div class="copy_rights">Copyright &copy; 2016 One-World Chemistry </div>
                     <a href="http://www.atomicka.com" target="_blank"> Development Partner Infom Atomicka Tech Pvt Ltd</a>
                     </div><!-- copy_right_holder -->
-               	 </div>       
+                 </div>       
             </footer>
         </div>
-	    <!--JS Inclution-->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script type="text/javascript" src="js/scripts.js"></script>
-	    <script type="text/javascript" src="js/jquery.min.js"></script>
-	    <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
-	    <script type="text/javascript" src="js/jquery-ui-1.10.3.custom.min.js"></script>  
-	    <script type="text/javascript" src="bootstrap-new/js/bootstrap.min.js"></script>
-	    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.tools.min.js"></script>
-	    <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
-	    <script type="text/javascript" src="js/jquery.scrollUp.min.js"></script>
-	    <script type="text/javascript" src="js/jquery.sticky.min.js"></script>
-	    
-	    <script type="text/javascript" src="js/jquery.flexisel.min.js"></script>
-
-	    
-	    <script type="text/javascript" src="js/custom.js"></script>
+        <!--JS Inclution-->
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+        <script type="text/javascript" src="js/jquery-ui-1.10.3.custom.min.js"></script>  
+        <script type="text/javascript" src="bootstrap-new/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/custom.js"></script>
         
         <script type='text/javascript'>
             $(window).load(function(){
